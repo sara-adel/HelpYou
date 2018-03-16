a22c10bbbc2dc3285606626d4c0160492e6b40ca
sara adel <saraadel895@gmail.com>
2018-03-16 18:22:05 +0200

    design

--- app/src/main/java/com/sara/helpyou/MainActivity.java
+++ app/src/main/java/com/sara/helpyou/MainActivity.java
@@ -1,13 +1,26 @@
 package com.sara.helpyou;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(MainActivity.this , Login.class);
+                startActivity(main);
+                finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
     }
 }
